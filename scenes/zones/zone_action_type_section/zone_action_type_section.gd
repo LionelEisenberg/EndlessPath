3e77f6445a2d293a9da29b1242e7ebf63f9165a7
Lionel Eisenberg <lionel.eisenberg@gmail.com>
2025-11-18 22:13:17 -0800

    Refactor and relocate adventure and combat scenes
    Moved adventure and combat scenes, scripts, and resources from 'scenes/game_systems' to top-level 'adventure', 'combat', and 'cycling' directories. Updated scene references and resource paths to match new locations. Added new adventure and combat scene files and supporting resources for modularity and improved project structure.

---
+++ scenes/zones/zone_action_type_section/zone_action_type_section.gd
@@ -0,0 +1,32 @@
+extends VBoxContainer
+
+@export var action_type : ZoneActionData.ActionType = ZoneActionData.ActionType.FORAGE
+@export var zone_action_data_list : Array[ZoneActionData] = []
+
+@onready var zone_action_button_scene : PackedScene = preload("res://scenes/zones/zone_action_button/zone_action_button.tscn")
+@onready var button_section: VBoxContainer = %ZoneActionButtonSection
+@onready var action_type_header_label: Label = %ActionTypeHeaderLabel
+
+func _ready() -> void:
+	if zone_action_data_list:
+		if button_section:
+			for child in button_section.get_children():
+				child.queue_free()
+		populate_actions(action_type, zone_action_data_list)
+		populate_header(action_type)
+
+func populate_actions(type: ZoneActionData.ActionType, action_list: Array[ZoneActionData]):
+	for action in action_list:
+		if action.action_type == type: 
+			var new_action_button = zone_action_button_scene.instantiate()
+			new_action_button.setup_action(action)
+			button_section.add_child(new_action_button)
+			
+func populate_header(type: ZoneActionData.ActionType):
+	match type:
+		ZoneActionData.ActionType.FORAGE:
+			action_type_header_label.text = "FORAGING ACTIONS"
+		ZoneActionData.ActionType.CYCLING:
+			action_type_header_label.text = "CYCLING ACTIONS"
+		ZoneActionData.ActionType.NPC_DIALOGUE:
+			action_type_header_label.text = "DIALOGUE ACTIONS"
